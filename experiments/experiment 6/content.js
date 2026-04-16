67e104edc0999514c4d96217b3e75ebee9b71b41
Devansh Pandey <pandeydevanshhhh@gmail.com>
2026-04-16 11:50:15 +0530

    16/04/2026 commit

--- experiments/experiment 6/content.js
+++ experiments/experiment 6/content.js
@@ -5,22 +5,40 @@ let input = document.getElementById("userInput");
 let fontSize = 16;
 
 // Change heading text (onclick using addEventListener)
-document.getElementById("changeTextBtn").addEventListener("click", function () {
-    if (input.value !== "") {
-        heading.innerHTML = input.value;
-    }
-});
-
-// Change background color
-document.getElementById("bgColorBtn").onclick = function () {
-    document.body.style.backgroundColor ="red";
-     
+// document.getElementById("changeTextBtn").addEventListener("click", function () {
+//     if (input.value !== "") {
+//         heading.innerHTML = input.value;
+//     }
+// });
+document.getElementById("changeTextBtn").onclick = function () {
+    heading.innerText = input.value;
 };
 
-// Increase font size
+input.onclick = function () {    
+    console.log("input changed:", input.value);
+};
+
+document.getElementById("bgColorBtn").addEventListener("click", function () {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+    document.body.style.backgroundColor = randomColor;
+    // document.body.style.backgroundColor ="lightblue";
+    document.body.style.fontFamily = "Arial, sans-serif";
+});
+
+// // Change background color
+// document.getElementById("bgColorBtn").onclick = function () {
+//     document.body.style.backgroundColor ="lightblue";
+     
+// };
+
+// // Increase font size
+// document.getElementById("fontSizeBtn").addEventListener("click", function () {
+//     fontSize += 2;
+//     paragraph.style.fontSize = fontSize + "px";
+// });
+
 document.getElementById("fontSizeBtn").addEventListener("click", function () {
-    fontSize += 2;
-    paragraph.style.fontSize = fontSize + "px";
+    heading.style.fontSize = 40 + "px";
 });
 
 // Show/Hide paragraph
@@ -41,3 +59,11 @@ document.getElementById("resetBtn").addEventListener("click", function () {
     input.value = "";
     fontSize = 16;
 });
+
+heading.onmouseover = function () {
+    heading.style.color = "red";
+};
+
+heading.onmouseout = function () {
+    heading.style.color = "black";
+};  
